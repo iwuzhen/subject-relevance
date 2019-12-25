58271ab2eef558c5334a8ee7589b23f29ca0824e
陆鸿飞 <lu.hongfei@shuzhi.ai>
2019-12-25 13:01:23 +0800

    主页修改

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="indexPage">
+    <div class="page-container page-component"></div>
+    <h1>wikipedia</h1>
     <div class="enterButton">
       <router-link to="/discipline">wiki学科相关度</router-link>
       <router-link to="/SubDiscipline">单个学科相关度</router-link>
       <router-link to="/powerlaw">幂律</router-link>
       <router-link to="/powerLawPageRank">PageRank幂律</router-link>
       <router-link to="/viewandedits">访问量&编辑量</router-link>
-
       <router-link to="/ArticlesTotal">wiki文章数查询</router-link>
+    </div>
+
+    <h1>MAS</h1>
+    <div class="enterButton">
       <router-link to="/masline">mas学科相关度</router-link>
       <router-link to="/MasArticlesTotal">mas文章数</router-link>
+    </div>
+    <h1>wikipedia 分类层次</h1>
+    <div class="enterButton">
+      <a href="http://wikidb.lambdax.cn:5555/wikidb_web/first.jsp">分类层次</a>
+    </div>
+    <h1>小世界</h1>
+    <div class="enterButton">
       <router-link to="/SmallWorldUndirected">小世界无向图</router-link>
       <router-link to="/SmallWorldDirected">小世界有向图</router-link>
-      <a href="http://wikidb.lambdax.cn:5555/wikidb_web/first.jsp">分类层次</a>
     </div>
   </div>
 </template>
@@ -23,31 +34,39 @@ export default {};
 
 <style lang="less">
 .indexPage {
+  text-align: left;
+  padding: 0 10vw;
+
+  h1 {
+    display: inline;
+  }
   .enterButton {
-    position: absolute;
+    // position: fixed;
     display: flex;
-    left: 50%;
-    top: 40%;
-    transform: translate(-50%, -50%);
-    a {
-      display: block;
-      height: 50px;
-      width: 8vw;
-      padding: 0 5px;
-      line-height: 50px;
-      text-align: center;
-      text-decoration: none;
-      color: #fff;
-      background: #409eff;
-      border-color: #409eff;
-      &:hover {
-        color: #409eff;
-        background: #ecf5ff;
-        border-color: #b3d8ff;
-      }
-      & + a {
-        margin-left: 20px;
-      }
+    margin: 10px 0 30px;
+
+    // left: 50%;
+    // top: 40%;
+    // transform: translate(-50%, -50%);
+  }
+  a {
+    display: block;
+    height: 50px;
+    width: 8vw;
+    padding: 0 5px;
+    line-height: 50px;
+    text-align: center;
+    text-decoration: none;
+    color: #fff;
+    background: #409eff;
+    border-color: #409eff;
+    &:hover {
+      color: #409eff;
+      background: #ecf5ff;
+      border-color: #b3d8ff;
+    }
+    & + a {
+      margin-left: 20px;
     }
   }
 }
