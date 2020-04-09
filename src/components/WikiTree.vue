75b1323bec55abf96196321e004526856c53ba5b
ider <lu.hongfei@outlook.com>
2020-04-09 11:14:47 +0800

    双列支持

--- src/components/WikiTree.vue
+++ src/components/WikiTree.vue
@@ -3,7 +3,7 @@
  * @Author: ider
  * @Date: 2020-04-08 11:55:19
  * @LastEditors: ider
- * @LastEditTime: 2020-04-08 19:22:20
+ * @LastEditTime: 2020-04-09 11:13:10
  * @Description: 
  -->
 <template>
@@ -11,12 +11,21 @@
     <div class="page-discipline">
       <div class="selectbox">
         <div class="selectitem">
+          <el-button type="primary" class="selectitem" @click="changeYear(1)"
+            >确定</el-button
+          >
+        </div>
+      </div>
+    </div>
+    <div class="wrapper">
+      <div>
+        <div class="selectitem">
           <span class="title">年份</span>
           <el-select
             v-model="yearSelect1"
             class="selectsubjectmiddle"
             placeholder="请选择"
-            @change="changeYear"
+            @change="changeYear(1)"
           >
             <el-option
               v-for="item in yearOptions"
@@ -26,23 +35,48 @@
             ></el-option>
           </el-select>
         </div>
+        <el-tree
+          v-if="openPanel1"
+          :props="props"
+          :load="loadNode1"
+          show-checkbox
+          accordion
+          lazy
+          node-key="id"
+          ref="tree1"
+        >
+        </el-tree>
+      </div>
+      <div>
         <div class="selectitem">
-          <el-button type="primary" class="selectitem" @click="changeYear"
-            >确定</el-button
+          <span class="title">年份</span>
+          <el-select
+            v-model="yearSelect2"
+            class="selectsubjectmiddle"
+            placeholder="请选择"
+            @change="changeYear(2)"
           >
+            <el-option
+              v-for="item in yearOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
         </div>
+        <el-tree
+          v-if="openPanel2"
+          :props="props"
+          :load="loadNode2"
+          show-checkbox
+          accordion
+          lazy
+          node-key="id"
+          ref="tree2"
+        >
+        </el-tree>
       </div>
     </div>
-    <el-tree
-      v-if="openPanel"
-      :props="props"
-      :load="loadNode"
-      accordion
-      lazy
-      node-key="id"
-      ref="tree"
-    >
-    </el-tree>
   </div>
 </template>
 
@@ -52,9 +86,10 @@ export default {
   name: "WikiTree",
   data() {
     return {
-      openPanel: true,
-      treeData: [],
+      openPanel1: true,
+      openPanel2: true,
       yearSelect1: 2020,
+      yearSelect2: 2019,
       wikiYearOptions: [
         2007,
         2008,
@@ -132,26 +167,24 @@ export default {
     }
   },
   methods: {
-    changeYear() {
-      // console.log(this.$refs.tree);
-      // for (let item of this.$refs.tree.root.childNodes) {
-      //   console.log(item.data);
-      //   let node = this.$refs.tree.getNode(item.data);
-      //   console.log(node);
-      //   // node.loaded = false;
-      //   // node.expand();
-      // }
-      this.openPanel = false;
-      setTimeout(() => {
-        this.openPanel = true;
-      }, 10);
+    changeYear(flag) {
+      if (flag === 1) {
+        this.openPanel1 = false;
+        setTimeout(() => {
+          this.openPanel1 = true;
+        }, 10);
+      } else {
+        this.openPanel2 = false;
+        setTimeout(() => {
+          this.openPanel2 = true;
+        }, 10);
+      }
     },
-    async loadNode(node, resolve) {
+    async _loadNode(node, resolve, _wikiDB) {
       if (node.level === 0) {
         return resolve(this.baseRegion);
       }
 
-      let _wikiDB = String(this.yearSelect1).slice(2, 4);
       if (node.label.indexOf("文章") > -1) {
         let response = await getWikiPageTree({
           categoryTitle: Buffer.from(node.parent.label).toString("base64"),
@@ -213,6 +246,14 @@ export default {
           }
         ]);
       }
+    },
+    async loadNode1(node, resolve) {
+      let _wikiDB = String(this.yearSelect1).slice(2, 4);
+      return await this._loadNode(node, resolve, _wikiDB);
+    },
+    async loadNode2(node, resolve) {
+      let _wikiDB = String(this.yearSelect2).slice(2, 4);
+      return await this._loadNode(node, resolve, _wikiDB);
     }
   }
 };
@@ -220,4 +261,18 @@ export default {
 
 <style lang="less" scoped>
 @import url("../assets/style/common.less");
+.wrapper {
+  display: grid;
+  // grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(2, 1fr);
+
+  gap: 1rem;
+  div {
+    background: #eee;
+    // padding: 1rem;
+  }
+  div:nth-child(odd) {
+    background: #ddd;
+  }
+}
 </style>
