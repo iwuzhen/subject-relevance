fab8ef869af92b99973b6c27d4682a8e3be29b35
lu.hongfei <lu.hongfei@outlook.com>
2021-11-16 17:12:17 +0800

    fix

--- src/pages/wikipediaBuild/CoreZipfByNodes_wiki.vue
+++ src/pages/wikipediaBuild/CoreZipfByNodes_wiki.vue
@@ -8,10 +8,10 @@ v-container(fluid)
       v-select(v-model='option.type.select', :items='option.type.opt', label='世界类型', @click='getData').
     v-col(cols='2')
       v-select(v-model='option.level.select', :items='option.level.opt', label='层次', @change='getData').
-    //- v-col(cols='2')
-    //-   v-select(v-model='option.year.select', :items='option.year.opt', label='幂律分布指定年', @click='getData').
-    //- v-col(cols='3')
-    //-   v-select(v-model='option.month.select', :items='option.month.opt', label='month', @click='getData').
+    v-col(cols='2')
+      v-select(v-model='option.year.select', :items='option.year.opt', label='幂律分布指定年', @click='getData').
+    v-col(cols='3')
+      v-select(v-model='option.month.select', :items='option.month.opt', label='month', @click='getData').
 
   v-row
     v-col(cols='11')
@@ -23,10 +23,10 @@ v-container(fluid)
   //-     v-range-slider.align-center(v-model='yearRange', :max='2021',thumb-label='always', :min='2007', dense, hide-details, label='学科趋势年份范围', hint='学科趋势年份范围', @change='getData')
   //-       template(v-slot:prepend)
   //-       template(v-slot:append)
-  //- v-row
-  //-   v-col(col='12')
-  //-     v-card
-  //-       v-data-table.elevation-1(:headers='girdHeaders', hide-default-footer, :items='gridData').
+  v-row
+    v-col(col='12')
+      v-card
+        v-data-table.elevation-1(:headers='girdHeaders', hide-default-footer, :items='gridData').
 
   v-row(v-for='chartid of myChartIds', :key='chartid')
     v-col(col='12')
@@ -84,11 +84,8 @@ export default {
       girdHeaders: [],
       option: {
         year: {
-          select: '',
-          opt: [{
-            text: '全部年份',
-            value: ''
-          }].concat(_.range(2004, 2022))
+          select: 2020,
+          opt: _.range(2004, 2022)
         },
         level: {
           select: 2,
@@ -120,14 +117,14 @@ export default {
             }
           ]
         },
-        nodeRange: [1, 30000],
+        nodeRange: [1, 10000],
         yearRange: [2004, 2021]
       },
       loading: false,
       chartOpt: {},
       chartData: {},
       gridData: [],
-      myChartIds: ['subjectChart1', 'subjectChart2']
+      myChartIds: ['subjectChart1', 'subjectChart2', 'subjectChart3']
     }
   },
   computed: {
@@ -178,10 +175,11 @@ export default {
           value: 'name'
         }
       ]
+      console.log(lines)
       for (const i in lines) {
         const myRegression = ecStat.regression(
           'linear',
-          lines[i].slice(this.nodeRange[0], this.nodeRange[1])
+          lines[i].slice(this.option.nodeRange[0], this.option.nodeRange[1])
         )
 
         retData[
@@ -205,30 +203,129 @@ export default {
         return false
       }
       this.loading = true
-      const opt = {
+      // chart 1
+      let opt = {
         cats: this.option.subject.select.join(','),
         type: this.option.type.select,
         year: this.option.year.select,
+        month: this.option.month.select,
         from_node: this.option.nodeRange[0],
         to_node: this.option.nodeRange[1],
         level: this.option.level.select
       }
+      try {
+        const res = await requestWrap('/wiki/getCoreZipfByNodes_newDB', 'POST', opt)
+        if (res.data) {
+          this.chartData = res.data
+          this.chartOpt = this.setOptions_chart1(res.data)
+          this.myChartObjs[0].setOption(this.chartOpt, true)
+          this.calSlope()
+        }
+      } catch (error) {
+        this.$emit('emitMesage', `请求失败:${error}`)
+        throw error
+      }
+
+      // chart 2,3
+      opt = {
+        cats: this.option.subject.select.join(','),
+        type: this.option.type.select,
+        year: '',
+        from_node: this.option.nodeRange[0],
+        to_node: this.option.nodeRange[1],
+        level: this.option.level.select
+      }
+      // console.log(mergeQuotaData, axios)
       axios.all([this.monthReq(opt, 3), this.monthReq(opt, 6), this.monthReq(opt, 9), this.monthReq(opt, 12)]).then(ret => {
         let data = mergeQuotaData(ret.map(obj => obj.data))
         this.chartOpt = this.setOptions(data, 'power law')
-        this.myChartObjs[0].setOption(this.chartOpt, true)
+        this.myChartObjs[1].setOption(this.chartOpt, true)
 
         data = mergeQuotaData(ret.map(obj => obj.data_node))
         console.log('allp:', data)
         this.chartOpt = this.setOptions(data, 'count')
-        this.myChartObjs[1].setOption(this.chartOpt, true)
+        this.myChartObjs[2].setOption(this.chartOpt, true)
       }).catch(() => {
         this.$emit('emitMesage', '请求失败')
       }).finally(() => {
         this.loading = false
-      }
-      )
+      })
     }, 2000),
+    setOptions_chart1(data) {
+      // let ymax = Math.max(...[].concat(...data.y));
+      // ymax = Math.ceil(ymax * 10) / 10;
+      let xmax = Math.max(...data.x)
+      xmax = Math.ceil(xmax * 10) / 10
+      // 设置
+      const seriesList = []
+      for (let i = 0; i < data.y.length; i++) {
+        const dataItem = []
+        for (let j = 0; j < data.y[i].length; j++) {
+          dataItem.push([data.x[j], data.y[i][j]])
+        }
+        seriesList.push({
+          name: data.legend[i],
+          type: 'scatter',
+          symbolSize: 5,
+          large: true,
+          data: dataItem
+        })
+      } // 排序;
+      seriesList.sort((x, y) => {
+        // console.log(y.data.slice(-1)[0][1], x.data.slice(-1)[0][1]);
+        return y.data.slice(-1)[0][1] - x.data.slice(-1)[0][1]
+      })
+
+      const _opt = extendEchartsOpts({
+        tooltip: {
+          trigger: 'axis',
+          textStyle: {
+            align: 'left'
+          },
+          axisPointer: {
+            type: 'cross',
+            animation: true,
+            label: {
+              backgroundColor: '#505765'
+            }
+          },
+          formatter: function(params) {
+            params.sort((x, y) => {
+              return y.data[1] - x.data[1]
+            })
+            let showHtm = ` x: ${params[0].data[0]},<br>`
+            for (let i = 0; i < params.length; i++) {
+              const _text = params[i].seriesName
+              const _data = params[i].data
+              const _marker = params[i].marker
+              showHtm += `${_marker}${_text}：${_data[1]} <br>`
+            }
+            tipLegend = params.map(item => item.seriesName)
+            return showHtm
+          }
+        },
+        title: {
+          text: data.title
+        },
+        legend: {
+          data: seriesList.map(item => {
+            return item.name
+          })
+        },
+        xAxis: {
+          type: 'value',
+          max: xmax,
+          name: 'log (rank)'
+        },
+        yAxis: {
+          type: 'value',
+          // max: ymax,
+          name: 'log (citation)'
+        },
+        series: seriesList
+      })
+      return _opt
+    },
     setOptions(data, yname) {
       // 设置
       const seriesList = _.zip(data.legend, data.y).map(item => {
